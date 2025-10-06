fb7599fdb195898b0e268e5993ed0f1859ff7417
VineMotapy <pythonmota@gmail.com>
2025-10-06 11:17:52 -0300

    Update parte1.py

--- parte1.py
+++ parte1.py
@@ -1,10 +1,8 @@
 # Inicio de estudo com Python envio de emails. 
 # Parte 1
-# - remetente: a conta de e-mail que vai enviar a mensagem
-# - senha_app: senha gerada no Gmail (senha de app), usada para autenticar no servidor
-# - destinatario: para quem o e-mail será enviado
-
-remetente = "remetente@gmail.com"
-senha_app = "******"  # senha de app criada no Gmail (não é a senha da conta)
-destinatario = "destinatariooutlook.com"
+# Dados do remetente e destinatário
+  
+remetente = "remetente@gmail.com"        # - remetente: a conta de e-mail que vai enviar a mensagem
+senha_app = "******"                     # senha de app criada no Gmail (não é a senha da conta) usada para autenticar no servidor
+destinatario = "destinatariooutlook.com" # para quem o e-mail será enviado
 
